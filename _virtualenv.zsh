df44ba7315e10dc8a4deb5836258d231db2f5644
Markus Törnqvist <mjt@nysv.org>
2018-11-12 15:59:16 +0200

    Look for virtualenvwrapper.sh in Cygwin

--- _virtualenv.zsh
+++ _virtualenv.zsh
@@ -6,7 +6,12 @@ export VIRTUAL_ENV_DISABLE_PROMPT=yes
 	VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
         VIRTUALENVWRAPPER=/usr/local/bin/virtualenvwrapper.sh
 } || {
-        VIRTUALENVWRAPPER=/usr/share/virtualenvwrapper/virtualenvwrapper.sh
+        # Cygwin, Windows
+        [ -f "${HOME}/../AppData/Local/Programs/Python/Python37/Scripts/virtualenvwrapper.sh" ] && {
+                VIRTUALENVWRAPPER="${HOME}/../AppData/Local/Programs/Python/Python37/Scripts/virtualenvwrapper.sh"
+        } || {
+                VIRTUALENVWRAPPER=/usr/share/virtualenvwrapper/virtualenvwrapper.sh
+        }
 }
 
 source $VIRTUALENVWRAPPER
