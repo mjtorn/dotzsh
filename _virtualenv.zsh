bc3cc6484b2e17ad2cf20fb69aa413feeed25813
Markus Törnqvist <mjt@nysv.org>
2018-11-13 11:25:08 +0200

    Windows hate

--- _virtualenv.zsh
+++ _virtualenv.zsh
@@ -1,18 +1,23 @@
 # precmd would fail this anyway
 export VIRTUAL_ENV_DISABLE_PROMPT=yes
 
-# OSX or Debian
-[ -f /usr/local/bin/virtualenvwrapper.sh ] && {
-	VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-        VIRTUALENVWRAPPER=/usr/local/bin/virtualenvwrapper.sh
-} || {
-        # Cygwin, Windows
-        [ -f "${HOME}/../AppData/Local/Programs/Python/Python37/Scripts/virtualenvwrapper.sh" ] && {
-                VIRTUALENVWRAPPER="${HOME}/../AppData/Local/Programs/Python/Python37/Scripts/virtualenvwrapper.sh"
-        } || {
-                VIRTUALENVWRAPPER=/usr/share/virtualenvwrapper/virtualenvwrapper.sh
-        }
-}
+# This file got out of hand.
+# Windows/Cygwin and virtualenvwrapper-win doesn't even work
 
-source $VIRTUALENVWRAPPER
+VENVWRAPBAT="${LOCALAPPDATA}/Programs/Python/Python37/Scripts/virtualenvwrapper.bat"
+[ $OS = "Windows_NT" ] && {
+	[ -f $VENVWRAPBAT ] && {
+		echo "Windows with virtualenvwrapper-win, not sourcing anything"
+	}
+} || {
+	# OSX or Debian
+	[ -f /usr/local/bin/virtualenvwrapper.sh ] && {
+		VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+		VIRTUALENVWRAPPER=/usr/local/bin/virtualenvwrapper.sh
+	} || {
+		VIRTUALENVWRAPPER=/usr/share/virtualenvwrapper/virtualenvwrapper.sh
+	}
+
+	source $VIRTUALENVWRAPPER
+}
 
